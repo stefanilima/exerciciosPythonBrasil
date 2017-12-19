db07fcab68afd9cc4261d6c95c396ec316511931
Stefani Lima <stefanisilvadelima@hotmail.com>
2017-12-19 11:58:00 -0200

    Exercício 05 da lista Python Brasil

---
+++ 1_EstruturaSequencial/exercicio05.py
@@ -0,0 +1,4 @@
+# 5 - Faça um Programa que converta metros para centímetros.
+metros = int(input('Digite a quantidade de metros: '))
+conversor = metros * 100
+print(metros, ' metros, são ', conversor, ' centimetros.')
